ed07c77c0550e1c287db04bb5f577210c512fa0f
Soren <IssacSWard@yahoo.com>
2020-10-29 22:11:48 -0400

    third update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 import PersonCard from './Components/NewComponent'
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <PersonCard firstName={"Jane"} lastName={"Doe"} age={"45"} hairColor={"Black"} />
-      <PersonCard firstName={"John"} lastName={"Smith"} age={"88"} hairColor={"Brown"} />
-      <PersonCard firstName={"Millard"} lastName={"Fillmore"} age={"50"} hairColor={"Brown"} />
-      <PersonCard firstName={"Maria"} lastName={"Smith"} age={"62"} hairColor={"Brown"} />
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+        user1:{
+            firstName:"Jane",
+            lastName:"Doe",
+            age:45,
+            hairColor:"Black"
+        },
+        user2:{
+          firstName:"John",
+          lastName:"Smith",
+          age:88,
+          hairColor:"Brown"
+        },
+        user3:{
+          firstName:"Millard",
+          lastName:"Fillmore",
+          age:50,
+          hairColor:"Brown"
+        },
+        user4:{
+          firstName:"Maria",
+          lastName:"Smith",
+          age:62,
+          hairColor:"Brown"
+      }
+    }
+}
+  render(){
+    return (
+      <div className="App">
+        <PersonCard user={this.state.user1} />
+        <PersonCard user={this.state.user2} />
+        <PersonCard user={this.state.user3} />
+        <PersonCard user={this.state.user4} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/NewComponent.js
+++ src/Components/NewComponent.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 
-class PersonCard extends Component{
+export default class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            age : this.props.user.age
+        }
+    }
+
     render(){
-        const { firstName, lastName, age, hairColor } = this.props;
         return(
         <div>
-        <h1>{lastName}, {firstName}</h1>
-        <p>Age: {age}</p>
-        <p>Hair Color: {hairColor}</p>
+            <h1>{this.props.user.lastName}, {this.props.user.firstName}</h1>
+            <p>Age: {this.state.age}</p>
+            <p>Hair Color: {this.props.user.hairColor}</p>
+            <button onClick={ () => this.setState({age: this.state.age+1}) }>Birthday Button for {this.props.user.firstName} {this.props.user.lastName}</button>
         </div>
         );
     }
 }
-
-export default PersonCard;
